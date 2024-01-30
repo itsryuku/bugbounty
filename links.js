7ecae3044e548513e2e4c23afe84c7e1a2cba215
ryukudz <malekmesdour@ryukudz.com>
2024-01-29 19:25:57 -0500

    ¯\_(ツ)_/¯

--- links.js
+++
@@ -1,1 +0,0 @@
-Array.from(document.querySelectorAll('script')).map(script => script.src || script.getAttribute('src')).filter(Boolean).forEach(link => console.log(`${link}\n`));
--- uuidgen.py
+++
@@ -1,5 +0,0 @@
-import uuid
-
-uuid = uuid.uuid4()
-string = str(uuid)
-print(string)
