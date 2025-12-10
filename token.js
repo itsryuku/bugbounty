79ade5d2b092a9f41a16c050f9621bdcfe952cbd
itsryuku <39871872+itsryuku@users.noreply.github.com>
2025-12-10 19:49:41 +0100

    ..

---
+++ token.js
@@ -0,0 +1,11 @@
+function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) {
+        return parts.pop().split(';').shift();
+    }
+    return null;
+}
+
+const token=getCookie('PA.ext');
+alert(`token: ${token}`)
